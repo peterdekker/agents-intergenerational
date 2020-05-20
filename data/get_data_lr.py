76d7c4b6df72e5ed260bee60d7097898295c00fc
Peter Dekker <peter.dekker@ai.vub.ac.be>
2020-05-20 11:09:59 +0200

    Updated data script

--- data/get_data_lr.py
+++ data/get_data_lr.py
@@ -7,9 +7,12 @@ start = time.time()
 lects_df = pd.read_csv(LECTS_PATH)
 lects_alorese = lects_df[lects_df["Name"].str.startswith("Alorese")]
 lects_austro = lects_df[lects_df["Family"].str.startswith("Austronesian")]
+lects_lamaholot_related = lects_df[lects_df["Name"].str.contains("Lamalera|Lewotobi|Lewoingu|Lewolema", case=False, regex=True)]
+lects_alorese_lamaholot = lects_alorese.append(lects_lamaholot_related)
 print(f"Total number of lects: {len(lects_df)}")
-print(f"Number of lects Alorese: {len(lects_alorese)}")
 print(f"Number of lects Austronese: {len(lects_austro)}")
+print(f"Number of lects Alorese: {len(lects_alorese)}")
+print(f"Number of lects Lamaholot lects related to Alorese: {len(lects_lamaholot_related)}")
 
 ### Read data
 data = pd.read_csv(DATA_PATH)
@@ -18,11 +21,11 @@ data = data[data["Concept_ID"].str.match(r"^\d.*")]
 # Join data with lect name from lects table
 data = data.merge(lects_df[["ID","Name"]], how="left", left_on="Lect_ID", right_on="ID")
 print(data)
-for langs, label in [(lects_alorese, "alorese"), (lects_austro, "austronesian")]:
+for langs, label in [(lects_austro, "austronesian"), (lects_alorese, "alorese"), (lects_alorese_lamaholot, "alorese-lamaholot")]:
     data_langs = data[data["Lect_ID"].isin(langs["ID"])]
     data_new = data_langs.pivot_table(index='Concept_ID', columns='Name', values='Form', aggfunc="/".join)
     order = ['1sg', '2sg_informal', '2sg_polite', '3sg', '1pl_excl', '1pl_incl', '2pl', '3pl' ]
-    #data_new = data_new.loc[order]
+    data_new = data_new.loc[order]
     data_new.to_csv(f"paradigm_{label}.tsv", sep="\t")
 
 end=time.time()
