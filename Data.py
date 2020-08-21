151d46c8b44b5f360326678ae5654a6d8c03259a
Peter Dekker <peter.dekker@ai.vub.ac.be>
2020-08-21 21:00:54 +0200

    change data

--- Data.py
+++ Data.py
@@ -8,7 +8,8 @@ class Data():
         self.persons = [col for col in self.data if re.match("[0-9]",col)]
 
         # Create transitivity dict
-        
+
 
 if __name__ == "__main__":
     d = Data("data/data.csv")
+
