Fixed stats calculation and enforced capacity in listen again

constants.py
@@ -3,7 +3,7 @@ import logging
 import sys
 from panphon.distance import Distance                                                                                                                 
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
 
 HEIGHT = 6
 WIDTH = 6
@@ -18,6 +18,7 @@ PERSONS = ['1sg', '2sg', '3sg', '1pl.incl', '1pl.excl', '2pl', '3pl']
 dst = Distance() 
 RG = np.random.default_rng()
 
+STATS_AFTER_ITERATIONS = 20
 
 # Defaults for UserSettableParameters
 SUFFIX_PROB = 0.5
@@ -26,4 +27,4 @@ CAPACITY_L1 = 50
 CAPACITY_L2 = 50
 DROP_SUBJECT_PROB = 0.0
 DROP_OBJECT_PROB = 0.0
-MIN_BOUNDARY_FEATURE_DIST = 0.0 # 2.0
+MIN_BOUNDARY_FEATURE_DIST = 3.0 # 2.0

stats.py
@@ -3,19 +3,6 @@ from itertools import combinations
 from distance import jaccard
 
 
-def morph_complexity(agent):
-    # TODO: optimize, get rid of loops
-    lengths = []
-    for lex_concept in agent.lex_concepts:
-        for person in agent.persons:
-            for affix_position in ["prefix", "suffix"]:
-                # Length is also calculated for empty affixes list (in L2 agents)
-                n_affixes = len(set(agent.affixes[(lex_concept, person, affix_position)]))
-                lengths.append(n_affixes)
-    mean_length = np.mean(lengths)  # if len(lengths)>0 else 0
-    return mean_length
-
-
 # TODO: optimize this by making fixed list of affixing and suffixing verbs
 def proportion_filled_entries(agent, aff_pos):
     total = 0
@@ -57,9 +44,8 @@ def global_dist(agents, lex_concepts, persons):
                         continue
                     jaccard_dist = jaccard(aff1, aff2)
                     dists.append(jaccard_dist)
-    global_model_distance = np.mean(dists)
-    return global_model_distance
+    return np.mean(dists) if len(dists) > 0 else 0
 
 def global_filled(agents, aff_pos):
-    filled_proportions = [proportion_filled_entries(a, aff_pos) for a in agents]
-    return np.mean(filled_proportions)
+    filled_props = [proportion_filled_entries(a, aff_pos) for a in agents]
+    return np.mean(filled_props) if len(filled_props) > 0 else 0